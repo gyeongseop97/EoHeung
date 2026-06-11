69095c0b1eae9a0555d24fc69e978b4d85e16d8e
gyeongseop97 <gyeongseop97@gmail.com>
2026-06-11 14:24:54 +0900

    Remove layout restore helper

--- layout-restore.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  function apply(){
-    var s=document.getElementById('eoEmergencyLayoutFix');
-    if(s) s.remove();
-    var st=document.getElementById('eoLayoutRestoreStyle');
-    if(st) st.remove();
-  }
-  if(document.readyState==='loading') document.addEventListener('DOMContentLoaded', apply); else apply();
-})();
